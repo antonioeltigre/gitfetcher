f7721d55b795e8fcfdf73332c4a9fce9e16acf37
simon.vezey <simon.vezey@activus.co.uk>
2018-03-22 14:05:37 +0000

    initial functionality

--- GitFetcher/Program.cs
+++ GitFetcher/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitFetcher
+﻿namespace GitFetcher
 {
-    class Program
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            do
+            {
+                Console.Out.WriteLine("doing a fetch");
+
+                var processInfo = GetProcessInfo(@"E:\Repo\GitActisure");
+
+                var process = Process.Start(processInfo);
+
+                LogProcessOutput(process);
+
+                Console.Out.WriteLine("fetch finished");
+                Wait();
+            }
+            while (true);
         }
+
+        private static ProcessStartInfo GetProcessInfo(string path)
+        {
+            return new ProcessStartInfo
+                       {
+                           UseShellExecute = false,
+                           RedirectStandardOutput = true,
+                           WorkingDirectory = path,
+                           FileName = @"C:\Windows\System32\cmd.exe",
+                           Verb = "runas",
+                           Arguments = "/c " + "git svn fetch",
+                           WindowStyle = ProcessWindowStyle.Hidden
+                       };
+        }
+
+        private static void Wait() => Thread.Sleep((int)TimeSpan.FromMinutes(1).TotalMilliseconds);
+
+        private static void LogProcessOutput(Process process) => Console.Out.WriteLine(process.StandardOutput.ReadToEnd());
     }
 }
